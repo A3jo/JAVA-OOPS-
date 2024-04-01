80db08fe2c6b4d79f6e871870dc47b26bdaea35d
A3jo <146429953+A3jo@users.noreply.github.com>
2024-04-01 11:47:58 +0530

    Update String_man.java

--- Cycle 2/String_man.java
+++ Cycle 2/String_man.java
@@ -6,7 +6,7 @@ System.out.println("Enter The String");
 Scanner sc = new Scanner(System.in);
 String str1 = sc.nextLine();
 System.out.println("Length of String = "+str1.length());
-System.out.println("Character at First position = "+str1.charAt(1));
+System.out.println("Character at First position = "+str1.charAt(0));
 System.out.println("String Contains 'o' sequence :"+str1.contains("o"));
 System.out.println("String ends with e : "+str1.endsWith("e"));
 System.out.println("Replace'o' with 'u' : "+str1.replaceAll("o","u"));
